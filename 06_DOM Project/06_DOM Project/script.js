fb4f74025a2b9effd0d7746d87a24aebaa3ff3bb
geetika-kalyankar <geetikakalyankar31@gmail.com>
2024-09-28 21:43:04 +0530

    updated!

---
+++ 06_DOM Project/06_DOM Project/script.js
@@ -0,0 +1,7 @@
+// First Assignment
+const logo = document.querySelector(".logo")
+logo.src = `assets/ineuron-logo.png`
+
+// Second Assignment
+const amount = document.querySelector(".amt")
+amount.innerHTML = "$10"
